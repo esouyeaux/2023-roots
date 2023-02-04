0a30ac104e3b46ecbd9639c0d3f98e3515077e1a
HydreigonGuy <ad.thibault18@gmail.com>
2023-02-04 02:06:29 +0100

    added dancing sprout

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -1,23 +1,32 @@
 import 'package:flame/components.dart';
+import 'package:flame/sprite.dart';
 import '../helpers/direction.dart';
  
-class Player extends SpriteComponent with HasGameRef {
+class Player extends SpriteAnimationComponent with HasGameRef {
   Direction direction = Direction.none;
   final double _playerSpeed = 300.0;
+  final double _animationSpeed = 0.15;
+  late final SpriteAnimation _standingAnimation;
 
  Player()
      : super(
          size: Vector2.all(100.0),
          angle: 1.57,
        );
- 
+
  @override
  Future<void> onLoad() async {
-   super.onLoad();
-   sprite = await gameRef.loadSprite('treeeeee.png');
-   position = gameRef.size / 2;
-
+  position = gameRef.size / 2;
+   _loadAnimations().then((_) => {animation = _standingAnimation});
+}
+Future<void> _loadAnimations() async {
+   final spriteSheet = SpriteSheet(
+     image: await gameRef.images.load('sapling.png'),
+     srcSize: Vector2(64.0, 64.0),
+   );
+ _standingAnimation = spriteSheet.createAnimation(row: 0, stepTime: _animationSpeed, to: 2);
  }
+
  @override
  void update(double delta) {
    super.update(delta);
